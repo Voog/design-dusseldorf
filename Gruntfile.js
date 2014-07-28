32aab3ff161686f694059227c2a5e720130acb8e
kaspar-naaber <kaspar.naaber@outlook.com>
2014-07-28 13:24:20 +0300

    Remove newer check from cssmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -149,7 +149,7 @@ module.exports = function(grunt) {
 
       css: {
         files: 'stylesheets/scss/*.scss',
-        tasks: ['sass', 'newer:cssmin'],
+        tasks: ['sass', 'cssmin'],
         options: {
           spawn: false
         }
